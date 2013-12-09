c432c0dd769fa951847fde09f79e4d6fdff411bc
Gowri Nambiar <gowri.nambi@gmail.com>
2013-12-09 18:18:30 -0500

    actually made clues controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Capicture::Application.routes.draw do
   
   resources :board do
-    resrouces :clue
+    resources :clue
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
