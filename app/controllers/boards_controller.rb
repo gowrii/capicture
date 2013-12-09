5a5e5a98ec268b48d858e48979eeb684b59d0b9a
Gowri Nambiar <gowri.nambi@gmail.com>
2013-12-09 18:42:59 -0500

    defining methods in board and clue controllers

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,2 +1,29 @@
 class BoardsController < ApplicationController
+
+	def show
+		@board = Board.find(params[:id])
+	end
+
+	def new
+		@board = Board.new
+	end
+
+	def create
+		@board = Board.new(board_params)
+		if @board.save
+			redirect_to board_path(@board)
+		else
+			render :new
+		end
+	end
+
+	def destroy
+	end
+
+	private
+
+	def board_params
+		params.require(:board).permit(:name)
+	end
+
 end
--- app/controllers/clues_controller.rb
+++ app/controllers/clues_controller.rb
@@ -1,6 +1,13 @@
 class CluesController < ApplicationController
-	
+	before_filter :load_board	
+
 	def show
+		@clue = Clue.find(params[:id])
+		@board = Board.find(parmas[:board_id])
 	end
 
+	private
+	def load_board
+		@board = Board.find(params[:board_id])
+	end
 end
