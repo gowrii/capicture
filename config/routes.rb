85702a8621bb671bf96deeb4744d04e32d236fa8
Stella Kim <stella.kim@icloud.com>
2013-12-11 15:59:55 -0500

    preparing for new routes and changes to board show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Capicture::Application.routes.draw do
     resources :clues
   end
 
+  # resources :clues do
+  #   resources :answers
+  # end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
