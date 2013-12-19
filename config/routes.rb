41ff6e6dc445ae277b20572b76c09bd442a632e8
Stella Kim <stella.kim@icloud.com>
2013-12-19 17:12:22 -0500

    navigation's site name is now a link back to root

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Capicture::Application.routes.draw do
   resources :users
   resources :sessions
 
+  root :to => 'games#index'
+
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "login"
   get "signup" => "users#new", :as => "signup"
