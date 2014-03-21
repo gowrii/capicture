80e3f7ecdf748d799fb6c58b23ad5ddc2a6ebc0b
Gowri Nambiar <gowri.nambi@gmail.com>
2014-03-20 23:01:52 -0400

    added bucket

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],                        # required
-    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"]                        # required
+    :aws_access_key_id      => ENV["S3_KEY"],                        # required
+    :aws_secret_access_key  => ENV["S3_SECRET"]                        # required
   }
-    config.fog_directory  = ENV["AWS_S3_BUCKET"]                     # required
+    config.fog_directory  = ENV["S3_BUCKET_NAME"]                     # required
 end
